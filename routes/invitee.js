ae6c7c62aeb1ba28f7f47c2de703f6352f14af45
MAC <mac@MACui-MacBook-Pro.local>
2018-01-21 20:59:51 +0900

    no message

--- routes/invitee.js
+++ routes/invitee.js
@@ -54,14 +54,14 @@ router.post('/invitee', auth.needsUserLogin, async (req, res, next) => {
 
 		// Coolsms sending message
 		try {
-			await coolsms.send(invitee.phone, "${invitee.name}님 안녕하세요. ${invitee.name}님을 알고 있는 ${req.user.nickname}라는 닉네임을 쓰는 분이 ${invitee.name}님과 1:1 대화를 하고 싶어서 대화방으로 초대를 하였습니다.
+			await coolsms.send(invitee.phone, ${invitee.name}님 안녕하세요. ${invitee.name}님을 알고 있는 ${req.user.nickname}라는 닉네임을 쓰는 분이 ${invitee.name}님과 1:1 대화를 하고 싶어서 대화방으로 초대를 하였습니다.
 익명채팅 앱 '누굴까'를 설치하면 ${req.user.nickname}님과 곧바로 채팅을 할 수 있습니다.
 설치 후 최초 로그인 시 아래의 인증코드를 입력해 주세요.
 인증코드: ${invitee.code}
 
 '누굴까'는 ‘초대한 사람은 상대방을 알지만 상대방은 초대한 사람을 모르는 채 서로 대화하는 익명 채팅 메신저' 입니다.
 http://admin.nuguga.kr/app/app
-", 'LMS')
+`, 'LMS')
 		} catch (err) {
 			return next(err)
 		}
@@ -89,14 +89,14 @@ http://admin.nuguga.kr/app/app
 
 		// Coolsms sending message
 		try {
-			await coolsms.send(invitee.phone, "${invitee.name}님 안녕하세요. ${invitee.name}님을 알고 있는 ${req.user.nickname}라는 닉네임을 쓰는 분이 ${invitee.name}님과 1:1 대화를 하고 싶어서 대화방으로 초대를 하였습니다.
+			await coolsms.send(invitee.phone, `${invitee.name}님 안녕하세요. ${invitee.name}님을 알고 있는 ${req.user.nickname}라는 닉네임을 쓰는 분이 ${invitee.name}님과 1:1 대화를 하고 싶어서 대화방으로 초대를 하였습니다.
 익명채팅 앱 '누굴까'를 설치하면 ${req.user.nickname}님과 곧바로 채팅을 할 수 있습니다.
 설치 후 최초 로그인 시 아래의 인증코드를 입력해 주세요.
 인증코드: ${invitee.code}
 
 '누굴까'는 ‘초대한 사람은 상대방을 알지만 상대방은 초대한 사람을 모르는 채 서로 대화하는 익명 채팅 메신저' 입니다.
 http://admin.nuguga.kr/app/app
-", 'LMS')
+`, 'LMS')
 		} catch (err) {
 			return next(err)
 		}
