be81b7b92a9644d609ea931cb8a48a578332ce94
choi hyoung jun <choihyoungjun@chois-MacBook-Pro.local>
2018-04-17 18:40:42 +0900

    no message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {port} = require('config').app
 const app = require('./app')
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, 'api.nuguga.kr', () => {
 	console.log(`Express server listening on port ${port}`)
 })
